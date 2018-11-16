8b847d969b79f14160e98ec51c35633c2147250c
SpectatorQL <hlcombine@gmail.com>
2018-11-16 19:27:40 +0100

    Initial commit

---
+++ src/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using static Berlin.Utils;
+
+namespace Berlin
+{
+    class Specimen
+    {
+        public int[] Gene;
+        public int GeneValue;
+
+        public void CalculateGeneValue(int[,] data)
+        {
+            int pathSum = 0;
+            int i = 0;
+            while(i < Gene.Length - 1)
+            {
+                int row = Gene[i];
+                int col = Gene[++i];
+                pathSum += data[row, col];
+            }
+            pathSum += data[i, 0];
+
+            GeneValue = pathSum;
+        }
+    }
+
+    class Program
+    {
+        static Random _rand = new Random();
+
+        const int _M = 40;
+        const int _K = -1;
+
+        static int[] GeneratePath(int len)
+        {
+            int[] result = new int[len];
+            for(int i = 0; i < len; ++i)
+            {
+                result[i] = i;
+            }
+
+            Shuffle(result);
+
+            return result;
+        }
+
+        static void Shuffle(int[] arr)
+        {
+            int len = arr.Length;
+            for(int i = 0; i < len; ++i)
+            {
+                int swap = _rand.Next(len);
+
+                int a = arr[i];
+                arr[i] = arr[swap];
+                arr[swap] = a;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string header;
+            int dataLen;
+            int[,] data;
+
+#if false
+            string file = "data\\berlinDebug.txt";
+#else
+            string file = "data\\berlin52.txt";
+#endif
+
+            using(FileStream stream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.Read))
+            using(StreamReader reader = new StreamReader(stream))
+            {
+                header = reader.ReadLine();
+                dataLen = int.Parse(header);
+                data = new int[dataLen, dataLen];
+
+                for(int i = 0; i < dataLen; ++i)
+                {
+                    string[] line = reader.ReadLine().Trim().Split(' ');
+
+                    for(int j = 0; j < i; ++j)
+                    {
+                        int val = int.Parse(line[j]);
+                        data[i, j] = val;
+                        data[j, i] = val;
+                    }
+                }
+            }
+
+            Specimen[] specimens = new Specimen[_M];
+            for(int i = 0; i < _M; ++i)
+            {
+                specimens[i] = new Specimen();
+                specimens[i].Gene = GeneratePath(dataLen);
+                specimens[i].CalculateGeneValue(data);
+
+#if true
+                Debug.WriteLine("specimen[{0}]: {1}", i, specimens[i].GeneValue);
+#endif
+            }
+
+#if false
+            Debug_PrintSquare(data, dataLen);
+#endif
+            Console.ReadKey();
+        }
+    }
+}
---
+++ src/Utils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Berlin
+{
+    class Utils
+    {
+        public static void Assert(bool expr)
+        {
+            if(!expr)
+                throw new NullReferenceException();
+        }
+
+        public static void Debug_PrintTriangle(int[,] arr, int rowLength)
+        {
+            for(int i = 0; i < rowLength; ++i)
+            {
+                for(int j = 0; j <= i; ++j)
+                {
+                    string printf = arr[i, j] + "   ";
+                    Debug.Write(printf);
+                }
+                Debug.WriteLine("");
+            }
+        }
+
+        public static void Debug_PrintSquare(int[,] arr, int rowLength)
+        {
+            for(int i = 0; i < rowLength; ++i)
+            {
+                for(int j = 0; j < rowLength; ++j)
+                {
+                    string printf = arr[i, j] + "   ";
+                    Debug.Write(printf);
+                }
+                Debug.WriteLine("");
+            }
+        }
+    }
+}
