b5543a9fd6fe9ffd63911414a50a77e9f90f97f2
SpectatorQL <hlcombine@gmail.com>
2018-12-14 20:38:41 +0100

    Implemented the OX crossover

--- src/Program.cs
+++ src/Program.cs
@@ -56,6 +56,55 @@ namespace Berlin
         {
         }
 
+        static int[] OXCrossover(int[] p1, int[] p2, int leftCut, int rightCut, int dataLen)
+        {
+            int[] child = new int[dataLen];
+
+            int crossLen = (rightCut - leftCut) + 1;
+            for(int i = leftCut;
+                i <= crossLen;
+                ++i)
+            {
+                child[i] = p1[i];
+            }
+
+            int j = rightCut + 1;
+            int nodesToCopy = dataLen - crossLen;
+            while(nodesToCopy > 0)
+            {
+                if(j >= dataLen)
+                {
+                    j = 0;
+                }
+
+                int node = p2[j];
+                if(!NodeIsInCrossSection(node, child, leftCut, rightCut))
+                {
+                    child[j] = node;
+                    --nodesToCopy;
+                }
+
+                ++j;
+            }
+
+            return child;
+        }
+
+        static bool NodeIsInCrossSection(int node, int[] arr, int crossStart, int crossEnd)
+        {
+            bool result = false;
+
+            for(int i = crossStart;
+                i < crossEnd;
+                ++i)
+            {
+                if(arr[i] == node)
+                    result = true;
+            }
+
+            return result;
+        }
+
         static bool IsDone()
         {
             // TODO: Actually do anything relevant.
@@ -74,7 +123,7 @@ namespace Berlin
             int[,] population;
             int[] fitnessValues;
 
-#if false
+#if true
             string file = "data\\berlinDebug.txt";
 #else
             string file = "data\\berlin52.txt";
@@ -161,6 +210,7 @@ namespace Berlin
 
                 int[] selected = new int[M];
                 // TODO: Make separate build configurations for the two.
+                // TODO: Or maybe invoke them based on a cmd argument?
 #if SELECTION_TOURNAMENT
                 TournamentSelection(selected, fitnessValues, M, K);
 #elif SELECTION_ROULETTE
@@ -171,39 +221,44 @@ namespace Berlin
                 int i = 0;
                 while(i < M)
                 {
-                    int[] child1 = null;
-                    int[] child2 = null;
-                    int c1 = i;
-                    int c2 = ++c1;
+                    int[] parent1 = new int[dataLen];
+                    int[] parent2 = new int[dataLen];
+                    int p1 = i;
+                    int p2 = p1 + 1;
 
                     for(int j = 0;
                         j < dataLen;
                         ++j)
                     {
-                        child1 = new int[dataLen];
-                        child2 = new int[dataLen];
-                        child1[j] = population[selected[c1], j];
-                        child2[j] = population[selected[c2], j];
+                        parent1[j] = population[selected[p1], j];
+                        parent2[j] = population[selected[p2], j];
                     }
 
                     int leftCut = -1;
                     int rightCut = -1;
-                    // TODO: Check if the cuts are apart from each other by some arbitrary length.
-                    while(leftCut < rightCut
-                        || rightCut < leftCut)
+                    // NOTE(SpectatorQL): I could probably use dataLen to calculate this. We'll see.
+#if true
+                    int dist = 1;
+#else
+                    int dist = 10;
+#endif
+                    while(dist > (rightCut - leftCut))
                     {
-                        leftCut = _rand.Next(0, dataLen);
-                        rightCut = _rand.Next(0, dataLen);
+                        int midPoint = dataLen / 2;
+                        leftCut = _rand.Next(1, midPoint);
+                        rightCut = _rand.Next(midPoint, dataLen - 1);
                     }
 
-
+                    // OX Crossover
+                    int[] child1 = OXCrossover(parent1, parent2, leftCut, rightCut, dataLen);
+                    int[] child2 = OXCrossover(parent2, parent1, leftCut, rightCut, dataLen);
 
                     for(int j = 0;
                         j < dataLen;
                         ++j)
                     {
-                        newPopulation[c1, j] = child1[j];
-                        newPopulation[c2, j] = child2[j];
+                        newPopulation[p1, j] = child1[j];
+                        newPopulation[p2, j] = child2[j];
                     }
 
                     i += 2;
@@ -212,6 +267,7 @@ namespace Berlin
                 population = newPopulation;
             }
             
+            // NOTE(SpectatorQL): Never gets here?
             Console.ReadKey();
         }
     }
